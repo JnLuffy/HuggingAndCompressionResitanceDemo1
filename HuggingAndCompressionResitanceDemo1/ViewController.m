6b6b8d42b824fc19ae10db01e4cb72d7a3da003f
JnLuffy <525444891@qq.com>
2017-02-16 17:59:12 +0800

    Hugging 和 CompressionResitances属性测试

--- HuggingAndCompressionResitanceDemo1/ViewController.m
+++ HuggingAndCompressionResitanceDemo1/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Masonry.h"
 
 @interface ViewController ()
 
@@ -16,14 +17,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self testContentCompressionResistance];
+    
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//测试空间的ContentCompressionResistance
+-(void)testContentCompressionResistance{
+    NSLog(@"aaaa");
+    UIButton * button1 = [UIButton buttonWithType:UIButtonTypeSystem];
+    [button1 setTitle:@"button 1 button 2" forState:UIControlStateNormal];
+    [button1 sizeToFit];
+    button1.backgroundColor = [UIColor redColor];
+    //设置ContentCompressionResistance的变化 设置下面代码后，button不会被压缩
+    
+//    [button1 setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisHorizontal];
+    [self.view addSubview:button1];
+    [button1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view).offset(100);
+        make.left.equalTo(self.view).offset(100);
+        make.right.equalTo(self.view).offset(-150).priorityHigh();
+        
+    }];
 }
+
 
 
 @end
