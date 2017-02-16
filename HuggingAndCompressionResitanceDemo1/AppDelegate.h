06f57bb568108244e21092a315d3ec55af4c6669
JnLuffy <525444891@qq.com>
2017-02-16 17:30:46 +0800

    Initial Commit

---
+++ HuggingAndCompressionResitanceDemo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HuggingAndCompressionResitanceDemo1
+//
+//  Created by IOS.Mac on 17/2/16.
+//  Copyright © 2017年 com.elepphant.pingchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HuggingAndCompressionResitanceDemo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HuggingAndCompressionResitanceDemo1
+//
+//  Created by IOS.Mac on 17/2/16.
+//  Copyright © 2017年 com.elepphant.pingchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
